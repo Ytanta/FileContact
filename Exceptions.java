ed6024d4cf2c8aa83de1f29a93099bba53cf19c9
Ytanta <121986594+Ytanta@users.noreply.github.com>
2023-11-07 20:22:18 +0300

    Add files via upload

---
+++ Exceptions.java
@@ -0,0 +1,21 @@
+package geekbrains.HomeWork;
+import java.io.IOException;
+
+
+
+    class IncorrectValueException extends RuntimeException{
+        public IncorrectValueException(String message) {
+            super(message);
+        }}
+    class EmptyStringException extends RuntimeException{
+        public EmptyStringException(String message) {
+            super(message);
+        }
+    }
+
+    class ArrayCountElementException extends RuntimeException{
+        public ArrayCountElementException(String message) {
+            super(message);
+        }
+    }
+
---
+++ Main.java
@@ -0,0 +1,25 @@
+package geekbrains.HomeWork;
+
+import java.io.IOException;
+
+
+    public class Main {
+        public static void main(String[] args) {
+
+            try {
+                DataGet.getData();
+
+            } catch (EmptyStringException e) {
+                System.out.println(e.getMessage());
+            } catch (ArrayCountElementException e) {
+                System.out.println(e.getMessage());
+            } catch (IncorrectValueException e) {
+                System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            };
+        }
+    }
+
